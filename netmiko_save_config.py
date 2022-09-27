1788bd1acd37a78113d8fd7915a47ec01067dbdb
mmuenste <64360180+mmuenste@users.noreply.github.com>
2022-09-27 14:13:13 +0200

    Add files via upload

---
+++ netmiko_save_config.py
@@ -0,0 +1,31 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+
+devices = [{'ip': '192.168.181.21',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.22',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.23',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.24',
+            'device_type': 'cisco_nxos'}
+           ]
+           
+user = input('Username: ')
+passw = getpass()
+
+for dev in devices:
+    net_connect = ConnectHandler(username=user,
+                                 password=passw,
+                                 **dev)
+    host = dev['ip']
+    print(host, '... connected')
+    config = net_connect.send_command('show run')
+    # print(config)
+    filename = host.replace(".", "_") + '_' + str(time()) + '.config'
+    cfg_file = open(filename, 'w')
+    for line in config:
+        cfg_file.write(line)
+    cfg_file.close()
+    net_connect.disconnect()
