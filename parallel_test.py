3b31ee03b9a117bb3ec2f081aba04e23647d2329
mmuenste <64360180+mmuenste@users.noreply.github.com>
2022-09-29 12:37:28 +0000

    Add files via upload

--- parallel_test.py
+++ parallel_test.py
@@ -5,37 +5,42 @@ import concurrent.futures
 from getpass import getpass
 import time
 
-def save_config(*args, **kwargs):
+def save_config(device:dict):
     
-    with ConnectHandler(args, kwargs) net_connect:
-        host = kwargs["ip"]
+    with ConnectHandler(**device) as net_connect:
+        host = device["ip"]
         print(f"{host}...connected")
         config = net_connect.send_command("show run")
-        filename = f"{host.replace('.', '_'}_{time.strftime()}"
+        filename = f"{host.replace('.', '_')}_{time.strftime('%Y%m%d_%H%M%S')}.config"
+        with open(filename, "w") as fobj:
+            fobj.write(config)
     
-    
-
 def main():
-    
-    devices = [{'ip': '192.168.181.21',
-            'device_type': 'cisco_nxos'},
-           {'ip': '192.168.181.22',
-            'device_type': 'cisco_nxos'},
-           {'ip': '192.168.181.23',
-            'device_type': 'cisco_nxos'},
-           {'ip': '192.168.181.24',
-            'device_type': 'cisco_nxos'}
-           ]
 
     user = input('Username: ')
     passw = getpass()
 
+    devices = [{'ip': '192.168.181.21',
+            'device_type': 'cisco_nxos',
+            'username': user,
+            'password': passw},
+           {'ip': '192.168.181.22',
+            'device_type': 'cisco_nxos',
+            'username': user,
+            'password': passw},
+           {'ip': '192.168.181.23',
+            'device_type': 'cisco_nxos',
+            'username': user,
+            'password': passw},
+           {'ip': '192.168.181.24',
+            'device_type': 'cisco_nxos',
+            'username': user,
+            'password': passw}
+           ]
     
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        e1 = executor.submit(pow, 2, 3)
-        e2 = executor.submit(pow, 2, 4)
+        e = executor.map(save_config, devices)
         
-    print(e1.result(), e2.result())
 
 if __name__ == '__main__':
     main()
